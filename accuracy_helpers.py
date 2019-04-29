0db7c1500a433b9ae3c2d6742e4a3f2b4515a9d5
David Holland <David.Holland@os.uk>
2019-04-29 15:09:22 +0100

    added norm_image cropping code

--- accuracy_helpers.py
+++ accuracy_helpers.py
@@ -4,4 +4,77 @@ Created on Mon Apr 29 10:51:48 2019
 
 @author: Jrainbow
 """
+import numpy as np
+import pandas as pd
+
+# HELPER FUNCTIONS
+def normalize(img):
+    """
+    Converts an image into binary form (1 when class is present)
+    Input: img array
+    Output: binary img array
+    """
+
+    mini = img.min()
+    
+    # Set negatives to 0
+    img[img == mini] = 0
+    
+    # Set positives to 1
+    img[img > mini] = 1
+    
+    out = img.astype(np.uint8)
+    return out
+
+def calc_averages(df):
+    """
+    Calculates the simple mean F1 score over each test tile and an alternative mean 
+    based on the average F1 score weighted by the number of True Positives present
+    Input: result dataframe from 'calc_stats'
+    Output: two percentages
+    """
+    F1 = df['F1']
+    F1_simple_average = np.mean(F1)
+    
+    df['weighted_F1'] = df['F1'] * df['True Positives']
+    F1_weighted_average = np.sum(df['weighted_F1']) / np.sum(df['True Positives'])
+    
+    print("F1 Simple Average: \t {:.2f}%".format(100 * F1_simple_average))
+    print("F1 Weighted Average: \t {:.2f}%".format(100 * F1_weighted_average))
+    
+    return F1_simple_average, F1_weighted_average
+
+def clip(cf_raster, gt_raster):
+    """
+    Calculates whether the classified raster is smaller than the grountruth raster or not 
+    and clips them to make sure they are the same size. TODO: what if vastly different size?
+    Input: classified raster, groundtruth raster (of varying shapes)
+    Output: classified raster, groundtruth raster (of identical shape)
+    TODO: actually want to crop to 3200 (or nearest multiple of 400)
+    """
+    cf_h, cf_w = cf_raster.shape
+    gt_h, gt_w = gt_raster.shape
+    min_h, min_w = np.min([cf_h, gt_h]), np.min([cf_w, gt_w])
+    smallest_raster = np.argmin([cf_h, gt_h])
+    if smallest_raster:
+        print("Groundtruth is Smaller")
+        cf_raster = cf_raster[: min_h, : min_w]
+    else:
+        print("Classified is Smaller")
+        gt_raster = gt_raster[: min_h, : min_w]
+    
+    return cf_raster, gt_raster
+
+def accuracy_metrics(raw):
+    """
+    Adds Recall, Precision and F1 scroes to the raw_matrix above
+    Input: raw_matrix
+    Output: same, with extra columns for Recall, Precision and F1
+    """
+    
+    raw['Recall'] = raw['True Positives'] / (raw['True Positives'] + raw['False Negatives'])
+    raw['Precision'] = raw['True Positives'] / (raw['True Positives'] + raw['False Positives'])
+    raw['F1'] = 2 * (raw['Precision'] * raw['Recall']) / (raw['Precision'] + raw['Recall'])
+    return raw
+
 
--- guyana_accuracy.py
+++ guyana_accuracy.py
@@ -16,84 +16,17 @@ import pandas as pd
 
 # Global Variables
 global RASTER_DIR 
-RASTER_DIR = r"O:\Guyana\shapefiles_test\rasterized"
+RASTER_DIR = r"O:\HackData\Classified"
 global CLASSIFIED_DIR
-CLASSIFIED_DIR = r"O:\tiled"
+CLASSIFIED_DIR = r"O:\HackData\Tiled"
 
-
-# HELPER FUNCTIONS
-def normalize(img):
-    """
-    Converts an image into binary form (1 when class is present)
-    Input: img array
-    Output: binary img array
-    """
-
-    mini = img.min()
-    
-    # Set negatives to 0
-    img[img == mini] = 0
-    
-    # Set positives to 1
-    img[img > mini] = 1
-    
-    out = img.astype(np.uint8)
-    return out
-
-def calc_averages(df):
-    """
-    Calculates the simple mean F1 score over each test tile and an alternative mean 
-    based on the average F1 score weighted by the number of True Positives present
-    Input: result dataframe from 'calc_stats'
-    Output: two percentages
-    """
-    F1 = df['F1']
-    F1_simple_average = np.mean(F1)
-    
-    df['weighted_F1'] = df['F1'] * df['True Positives']
-    F1_weighted_average = np.sum(df['weighted_F1']) / np.sum(df['True Positives'])
-    
-    print("F1 Simple Average: \t {:.2f}%".format(100 * F1_simple_average))
-    print("F1 Weighted Average: \t {:.2f}%".format(100 * F1_weighted_average))
-    
-    return F1_simple_average, F1_weighted_average
-
-def clip(cf_raster, gt_raster):
-    """
-    Calculates whether the classified raster is smaller than the grountruth raster or not 
-    and clips them to make sure they are the same size. TODO: what if vastly different size?
-    Input: classified raster, groundtruth raster (of varying shapes)
-    Output: classified raster, groundtruth raster (of identical shape)
-    TODO: actually want to crop to 3200 (or nearest multiple of 400)
-    """
-    cf_h, cf_w = cf_raster.shape
-    gt_h, gt_w = gt_raster.shape
-    min_h, min_w = np.min([cf_h, gt_h]), np.min([cf_w, gt_w])
-    smallest_raster = np.argmin([cf_h, gt_h])
-    if smallest_raster:
-        print("Groundtruth is Smaller")
-        cf_raster = cf_raster[: min_h, : min_w]
-    else:
-        print("Classified is Smaller")
-        gt_raster = gt_raster[: min_h, : min_w]
-    
-    return cf_raster, gt_raster
-
-def accuracy_metrics(raw):
-    """
-    Adds Recall, Precision and F1 scroes to the raw_matrix above
-    Input: raw_matrix
-    Output: same, with extra columns for Recall, Precision and F1
-    """
-    
-    raw['Recall'] = raw['True Positives'] / (raw['True Positives'] + raw['False Negatives'])
-    raw['Precision'] = raw['True Positives'] / (raw['True Positives'] + raw['False Positives'])
-    raw['F1'] = 2 * (raw['Precision'] * raw['Recall']) / (raw['Precision'] + raw['Recall'])
-    return raw
-
+from accuracy_helpers import normalize, calc_averages, clip, accuracy_metrics
+  
 
 class GuyanaTile:
-        
+    STRIDE = 200
+    SIZE = 400
+    NORM = 0
     def __init__(self, code, class_):
         self.__code = code
         self.__class = class_
@@ -107,22 +40,48 @@ class GuyanaTile:
     def read_groundtruth_raster(self):
         raster_path = self.get_raster_path()
         raster = imread(raster_path)
+        raster = self.norm_image(raster)
         return raster
     
     def get_classified_folder(self, appendage='HACK_IRG1'):
         TILE_DIR = os.path.join(self.__classified_dir, "{}_{}".format(self.__code, appendage))
         return TILE_DIR
     
-    def get_classified_raster_path(self, appendage='HACK_IRG1', extras='', norm=2):
+    def get_classified_raster_path(self, appendage='HACK_IRG1', extras='', norm=NORM):
         TILE_DIR = self.get_classified_folder(appendage=appendage)
         OUTPUT_DIR = os.path.join(TILE_DIR, '{}_masks{}'.format(self.__class, str(extras)))
         raster_path = os.path.join(OUTPUT_DIR, '{}_normalized_{}.tif'.format(self.__code, str(norm)))
         return raster_path
     
-    def read_classified_raster(self, appendage='HACK_IRG1', extras='', norm=2):
+    def calc_limits(self,raster):
+        h,w = raster.shape
+        w = STRIDE*int(w/STRIDE)
+        h = STRIDE*int(h/STRIDE)
+        return h,w
+        
+        
+    def norm_image(self,img):
+        im_height, im_width = self.calc_limits(img)
+        h0 = STRIDE
+        h1 = im_height - STRIDE
+        w0 = STRIDE
+        w1 = im_width - STRIDE
+        if h0>0 and w0>0 and h1>h0 and w1>w0:
+            img = img[h0:h1,w0:w1]
+        else:
+            print("raster data too small to clip.  Setting to zero")
+            img = raster[0:0,0:0]
+        return img
+        
+        
+    def read_classified_raster(self, appendage='HACK_IRG1', extras='', norm=NORM):
         raster_path = self.get_classified_raster_path(appendage=appendage, extras=extras, norm=norm)
         raster = imread(raster_path)
+        if norm != 0:
+            raster = self.norm_image(raster)
+            
         return raster
+    
         
     def calc_stats(self, appendage='HACK_IRG1',extras=''):
         
@@ -134,7 +93,7 @@ class GuyanaTile:
         fn = []
         
         gt = self.read_groundtruth_raster()
-        cf = self.read_classified_raster(appendage=appendage, extras=extras, norm=2)
+        cf = self.read_classified_raster(appendage=appendage, extras=extras)
         
         gt_norm = normalize(gt)
         cf_norm = normalize(cf)
@@ -173,7 +132,6 @@ class GuyanaTile:
         
         return accuracy
     
-  
 def main():
 
     testTiles = ['GT2031', 'GT1033', 'GT1014']
